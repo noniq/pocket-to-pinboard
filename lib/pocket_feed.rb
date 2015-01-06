5335e7b09867107bfae23fba4518ee1c0d72c94b
Stefan Daschek <stefan@daschek.net>
2015-01-06 22:44:04 +0100

    Use HTTPS when accessing pocket feed.

--- lib/pocket_feed.rb
+++ lib/pocket_feed.rb
@@ -10,7 +10,7 @@ class PocketFeed
   
   def feed
     @feed ||= begin
-      response = HTTParty.get("http://getpocket.com/users/noniq/feed/all", basic_auth: { username: @username, password: @password })
+      response = HTTParty.get("https://getpocket.com/users/noniq/feed/all", basic_auth: { username: @username, password: @password })
       raise "Error loading RSS feed: #{response.body}" unless response.code == 200
       RSS::Parser.parse(response)
     end
